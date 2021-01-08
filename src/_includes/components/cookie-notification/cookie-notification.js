7e6d3cb3d6258ac570d275f108620d878b22ebba
Declan Rek <declanrek@gmail.com>
2021-01-08 16:38:16 +0100

    always show cookie banner

--- src/_includes/components/cookie-notification/cookie-notification.js
+++ src/_includes/components/cookie-notification/cookie-notification.js
@@ -1,19 +1,12 @@
 import cookie from '../cookie/cookie'
 
-const ACTIVE_CLASS = 'cookie-notification--is-active'
+const HIDDEN_CLASS = 'cookie-notification--is-hidden'
 const cookieNotificationElement = document.querySelector('[data-cookie-notification]')
 const acceptButton = document.querySelector('[data-cookie-notification-accept-button')
 
 function initCookieNotification() {
   if(!cookieNotificationElement) {
     return
-  }
-
-  if (!cookie('cookie-consent-seen')) {
-    setTimeout(() => {
-
-    showOffscreenMessage(cookieNotificationElement)
-    }, 1500)
   }
 
   acceptButton.addEventListener('click', onAccept)
@@ -24,16 +17,9 @@ function onAccept() {
   hideOffscreenMessage(cookieNotificationElement)
 }
 
-export function showOffscreenMessage(element) {
-  element.removeAttribute('aria-hidden')
-  element.classList.add(ACTIVE_CLASS)
-  element.setAttribute('role', 'alert')
-  element.setAttribute('aria-live', 'assertive')
-}
-
 export function hideOffscreenMessage(element) {
   element.setAttribute('aria-hidden', 'true')
-  element.classList.remove(ACTIVE_CLASS)
+  element.classList.add(HIDDEN_CLASS)
   element.removeAttribute('role')
   element.removeAttribute('aria-live')
 }
